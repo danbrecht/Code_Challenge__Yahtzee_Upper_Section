4b79e7ecfaa0dc993244f038e4832d5cd461a6c8
Dan <Dan@DESKTOP-VUVTTTU>
2019-12-14 12:08:03 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yahtzee_Upper_Section
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
